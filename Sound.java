2a6f735a42b78bf1fea6481d79893698f69e31a4
lhao3 <91486184+lhao3@users.noreply.github.com>
2021-10-14 13:04:01 -0400

    Add files via upload

---
+++ Sound.java
@@ -0,0 +1,30 @@
+import javax.sound.midi.*;
+
+public class Sound
+{
+   protected static MidiChannel[] channels = null;
+   protected static Instrument[] instr;
+   protected static final int PIANO = 0;
+   protected static final int VIOLIN = 41;
+   protected static final int CELLO = 43;
+   
+   public static void initialize()
+   {
+      try
+      {
+         Synthesizer synth = MidiSystem.getSynthesizer();
+         synth.open();
+         Sound.channels = synth.getChannels();
+         Sound.instr = synth.getDefaultSoundbank().getInstruments();
+      }
+      catch(Exception ignored)
+      {}
+      channels[0].programChange(instr[PIANO].getPatch().getProgram());
+      silence();
+   }
+   
+   public static void silence()
+   {
+      channels[0].allNotesOff();
+   }
+}
