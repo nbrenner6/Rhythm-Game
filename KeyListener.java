181620aab4aa3961227913ff883351986a25c624
lhao3 <91486184+lhao3@users.noreply.github.com>
2022-03-23 11:44:56 -0400

    Update KeyListener.java

--- KeyListener.java
+++ KeyListener.java
@@ -5,9 +5,58 @@ public class KeyListener extends KeyAdapter
 {
    public void keyPressed(KeyEvent x)
    {
+      if(Menu.game == null)
+      {
+         return;
+      }
+      
       if(x.getKeyCode() == KeyEvent.VK_W)
       {
-      
+         Menu.game.pressW();
       }
+      
+      if(x.getKeyCode() == KeyEvent.VK_A)
+      {
+         Menu.game.pressA();
+      }
+      
+      if(x.getKeyCode() == KeyEvent.VK_S)
+      {
+         Menu.game.pressS();
+      }
+      
+      if(x.getKeyCode() == KeyEvent.VK_D)
+      {
+         Menu.game.pressD();
+      }
+   }
+   
+   public void keyReleased(KeyEvent x)
+   {
+      if(Menu.game == null)
+      {
+         return;
+      }
+      
+      if(x.getKeyCode() == KeyEvent.VK_W)
+      {
+         Menu.game.releaseW();
+      }
+      
+      if(x.getKeyCode() == KeyEvent.VK_A)
+      {
+         Menu.game.releaseA();
+      }
+      
+      if(x.getKeyCode() == KeyEvent.VK_S)
+      {
+         Menu.game.releaseS();
+      }
+      
+      if(x.getKeyCode() == KeyEvent.VK_D)
+      {
+         Menu.game.releaseD();
+      }
+
    }
 }
