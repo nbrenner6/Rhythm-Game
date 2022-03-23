b091f5dcb11aa0f0023a8529bdd68a8a8c8b259e
lhao3 <91486184+lhao3@users.noreply.github.com>
2022-03-23 11:32:07 -0400

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,58 @@
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+public class Game
+{
+   private String gameTitle;
+
+   public Game(String title)
+   {
+      gameTitle = title;
+   }
+   
+   public void pressW()
+   {
+      
+   }
+   
+   public void releaseW()
+   {
+   
+   }
+   
+   public void pressA()
+   {
+   
+   }
+   
+   public void releaseA()
+   {
+   
+   }
+   
+   public void pressS()
+   {
+   
+   }
+   
+   public void releaseS()
+   {
+   
+   }
+   
+   public void pressD()
+   {
+   
+   }
+   
+   public void releaseD()
+   {
+   
+   }
+   
+   public void accuracyCheck(String p)
+   {
+      
+   }
+
+}
---
+++ KeyListener.java
@@ -0,0 +1,13 @@
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyListener extends KeyAdapter
+{
+   public void keyPressed(KeyEvent x)
+   {
+      if(x.getKeyCode() == KeyEvent.VK_W)
+      {
+      
+      }
+   }
+}
