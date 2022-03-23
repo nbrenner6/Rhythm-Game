e29874d3bc0ecb76aaa8771c9b5704512de6f18f
lhao3 <91486184+lhao3@users.noreply.github.com>
2022-03-23 11:32:35 -0400

    Update Note.java

--- Note.java
+++ Note.java
@@ -1,8 +1,13 @@
-/*public class Note{
+import java.awt.Image;
+import java.awt.Graphics2D;
+import javax.swing.ImageIcon;
+
+public class Note extends Thread{
    private String direction;
    private double time;
-   private x;
-   private y;
+   private int x = 250 - (1000/Menu.sleepTime*Menu.noteSpeed)*Menu.reachTime;
+   private int y = 250 - (1000/Menu.sleepTime*Menu.noteSpeed)*Menu.reachTime;;
+   private Image arrowImage = new ImageIcon(Menu.class.getResource("../images/downarrow.png")).getImage();
    
    
    public Note(String d, int t){
@@ -26,9 +31,45 @@
       time = t;  
    }
    
+   public void createNote(Graphics2D g)
+   {
+      g.drawImage(arrowImage, x, y, null);
+   }
    
+   public String noteTiming()
+   {
+      if(y <= 200)
+      {
+         System.out.println("Too Early");
+         return "Too Early";
+      }
+      else if(y <= 225)
+      {
+         System.out.println("Good");
+         return "Good";
+      }
+      else if(y <= 275)
+      {
+         System.out.println("Perfect");
+         return "Perfect";
+      }
+      else if(y <= 325)
+      {
+         System.out.println("Good");
+         return "Good";
+      }
+      else if(y <= 375)
+      {
+         System.out.println("Too Late");
+         return "Too Late";
+      }
+      else
+      {
+         return "None";
+      }
+      
+   }
    
    
 }
 
-*/
