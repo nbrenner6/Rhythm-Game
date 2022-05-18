63a4f7399a8419a610305553bdb8eef886531937
lhao3 <91486184+lhao3@users.noreply.github.com>
2022-05-18 12:43:03 -0400

    Update RhythmGameDriver.java

--- RhythmGameDriver.java
+++ RhythmGameDriver.java
@@ -7,13 +7,14 @@ public class RhythmGameDriver
    public static Menu screen;
    
    public static void main(String args[]){
-      Menu test = new Menu();
+      Menu screen = new Menu();
       JFrame MainFrame = new JFrame("RHYTHM-GAME");
       MainFrame.setSize(1100, 1100);
       MainFrame.setLocation(155, 30);
       MainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      MainFrame.setContentPane(test);
+      MainFrame.setContentPane(screen);
       MainFrame.setVisible(true);
+      MainFrame.addKeyListener(screen);
 
    }
 }
